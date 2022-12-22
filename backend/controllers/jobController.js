2bf30f3156ead3ad2e4a92d5240d91d959fdc09b
draganvasilj52 <draganvasilj28@gmail.com>
2022-12-22 15:19:00 +0100

    added company and user routes 1

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -8,67 +8,6 @@ const getAllJobs = async (req, res) => {
     jobs,
   })
 }
-/*
-const createPlace = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
-    );
-  }
-
-  const { title, description, address, creator } = req.body;
-
-  let coordinates;
-  try {
-    coordinates = await getCoordsForAddress(address);
-  } catch (error) {
-    return next(error);
-  }
-
-   const createdPlace = new Place({
-    title,
-    description,
-    address,
-    location: coordinates,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg',
-    creator
-  });
-
-  let user;
-  try {
-    user = await User.findById(creator);
-  } catch (err) {
-    const error = new HttpError('Creating place failed, please try again', 500);
-    return next(error);
-  }
-
-  if (!user) {
-    const error = new HttpError('Could not find user for provided id', 404);
-    return next(error);
-  }
-
-  console.log(user);
-
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await createdPlace.save({ session: sess });
-    user.places.push(createdPlace);
-    await user.save({ session: sess });
-    await sess.commitTransaction();
-  } catch (err) {
-    const error = new HttpError(
-      'Creating place failed, please try again.',
-      500
-    );
-    return next(error);
-  }
-
-  res.status(201).json({ place: createdPlace });
-};
- */
 
 const createJob = async (req, res) => {
   const {
